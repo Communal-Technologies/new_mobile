6e16dc6cd3f777541c4c4b05d27b5b8127e19606
Master Daniel <engrdanywiss@gmail.com>
2025-06-21 16:39:11 +0100

    Adds image asset constants for app resources
    Introduces a new class to centralize the management of image asset paths.
    Facilitates consistency and ease of maintenance by providing a single
    source of truth for image references used throughout the application.

---
+++ lib/core/constants/images.dart
@@ -0,0 +1,25 @@
+class Images {
+  static const String cake = 'assets/images/cake.png';
+  static const String coloredLogo = 'assets/images/logo-02.png';
+  static const String whiteLogo = 'assets/images/logo-01.png';
+  static const String whiteIcon = 'assets/images/icon-01.png';
+  static const String coloredIcon = 'assets/images/icon-02.png';
+  static const String onBoardingOne = 'assets/images/onboarding_one.png';
+  static const String onBoardingTwo = 'assets/images/onboarding_two.png';
+  static const String onBoardingThree = 'assets/images/onboarding_three.png';
+  static const String unitedKingdom = 'assets/images/united_kindom.png';
+  static const String maintenance = 'assets/images/maintenance.png';
+  static const String background = 'assets/images/background.png';
+  static const String apple = 'assets/images/apple.png';
+  static const String google = 'assets/images/google.png';
+  static const String verifySuccess = 'assets/images/verify_success.png';
+  static const String clappingHands = 'assets/images/clapping_hands.png';
+  static const String community = 'assets/images/community.png';
+  // static const String demoUser = 'assets/images/demo_user.png';
+  static const String fund = 'assets/images/funds.png';
+  static const String withdraw = 'assets/images/withdraw.png';
+  static const String wifi = 'assets/images/wifi.png';
+  static const String send = 'assets/images/send.png';
+  static const String qrcode = 'assets/images/qrcode.png';
+  static const String update = 'assets/images/update.png';
+}

a1afabfedaa06ee83e7176abcd52ab2dae25174d
Master Daniel <engrdanywiss@gmail.com>
2025-06-21 16:32:21 +0100

    Add authentication events for login flow
    Introduces a set of authentication events to manage the login and
    logout processes. These events include app start, login request
    with credentials, logout request, and a check for authentication
    status, facilitating a structured approach to handle authentication
    actions in the application.

---
+++ lib/blocs/auth/auth_event.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AppStarted extends AuthEvent {}
+
+class LoginRequested extends AuthEvent {
+  final String login;
+  final String password;
+
+  const LoginRequested({required this.login, required this.password});
+
+  @override
+  List<Object?> get props => [login, password];
+}
+
+class LogoutRequested extends AuthEvent {}
+
+class CheckAuthStatus extends AuthEvent {}

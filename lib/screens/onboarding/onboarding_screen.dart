7c7681d98d154b66c53fe407b8cfba43c5c7533c
Master Daniel <engrdanywiss@gmail.com>
2025-10-04 14:05:50 +0100

    Adds onboarding screen
    Introduces an onboarding screen to guide new users through the app's features and benefits.
    
    The onboarding screen consists of a series of pages with descriptive text and images. It persists a flag in shared preferences to indicate that the user has completed the onboarding process, and then navigates to the welcome screen.

---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:communal_mobile/core/constants/images.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:communal_mobile/core/widgets/space.dart';
+import 'package:communal_mobile/core/widgets/app_elevated_button.dart';
+import 'package:communal_mobile/screens/onboarding/widgets/indicator.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  final List<Map<String, String>> onboardingData = [
+    {
+      'text': 'Create a shared financial adventure',
+      'image': Images.onBoardingOne,
+    },
+    {'text': 'Your new financial playground', 'image': Images.onBoardingTwo},
+    {'text': 'Find your Community', 'image': Images.onBoardingThree},
+  ];
+
+  void _onNextPressed() {
+    if (_currentIndex < onboardingData.length - 1) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 400),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _completeOnboarding();
+    }
+  }
+
+  void _onSkip() {
+    _completeOnboarding();
+  }
+
+  void _completeOnboarding() async {
+    // Persist onboarding complete flag
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('first_time', false);
+
+    if (!mounted) return;
+    Navigator.pushReplacementNamed(
+      context,
+      '/welcome',
+    ); // GoRouter can be used here too
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: onboardingData.length,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              itemBuilder: (context, index) {
+                return OnboardingPage(
+                  index: index,
+                  text: onboardingData[index]['text']!,
+                  image: onboardingData[index]['image']!,
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                vSpace(10),
+                OnboardingIndicator(
+                  currentIndex: _currentIndex,
+                  total: onboardingData.length,
+                ),
+                vSpace(20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _currentIndex != onboardingData.length - 1
+                        ? AppElevatedButton(
+                            title: 'Skip',
+                            onPressed: _onSkip,
+                          )
+                        : const SizedBox.shrink(),
+                    hSpace(10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: _onNextPressed,
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            Theme.of(context).primaryColor,
+                          ),
+                          foregroundColor: WidgetStateProperty.all<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                        child: Text(
+                          _currentIndex == onboardingData.length - 1
+                              ? "Let's Go"
+                              : 'Next',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  final int index;
+  final String text;
+  final String image;
+
+  const OnboardingPage({
+    super.key,
+    required this.index,
+    required this.text,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: Image.asset(
+                    Images.coloredLogo,
+                    height: 71,
+                    width: 250,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: index == 2 ? 50 : 60,
+                    vertical: 20,
+                  ),
+                  child: Text(
+                    text,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                      fontSize: index == 2 ? 48 : 24,
+                      color: const Color(0xFF3E28A2),
+                      shadows: index == 2
+                          ? [
+                              const BoxShadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.19),
+                                offset: Offset(11, 10),
+                                blurRadius: 10,
+                              ),
+                            ]
+                          : null,
+                    ),
+                  ),
+                ),
+                Image.asset(image, height: 300, width: 300),
+              ],
+            ),
+          ),
+          if (index == 2)
+            Positioned(
+              top: 158,
+              left: 0,
+              child: Image.asset(Images.cake, width: 70, height: 52),
+            ),
+          if (index == 2)
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(Images.cake, width: 70, height: 52),
+            ),
+        ],
+      ),
+    );
+  }
+}

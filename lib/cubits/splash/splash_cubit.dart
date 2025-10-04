94af4c7c48830413888967ea0e8738841d1962af
Master Daniel <engrdanywiss@gmail.com>
2025-10-04 11:26:35 +0100

    Fetches and sets app settings on init
    Fetches settings from the remote configuration and stores them
    in the app-level state using the SettingsCubit. This allows
    the app to dynamically configure itself based on the remote
    settings. Also passes settings to logged in state.

--- lib/cubits/splash/splash_cubit.dart
+++ lib/cubits/splash/splash_cubit.dart
@@ -3,13 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:communal_mobile/data/datasources/remote/dio/dio_client.dart';
+import 'package:communal_mobile/cubits/settings/settings_cubit.dart';
+import 'package:communal_mobile/data/models/settings_model.dart';
 import 'splash_state.dart';
 
 class SplashCubit extends Cubit<SplashState> {
   final SharedPreferences prefs;
   final DioClient dioClient;
+  final SettingsCubit settingsCubit;
 
-  SplashCubit(this.prefs, this.dioClient) : super(SplashInitial());
+  SplashCubit(this.prefs, this.dioClient, this.settingsCubit)
+    : super(SplashInitial());
 
   Future<void> initApp() async {
     emit(SplashLoading());
@@ -26,15 +30,16 @@ class SplashCubit extends Cubit<SplashState> {
 
       final response = await dioClient.get(AppConstants.configUri);
       final settings = response.data;
-
-      // TODO: Store `settings` to app-level state management if needed
-
+      final settingsResponse = SettingsResponse.fromJson(settings);
+      final settingsMap = settingsResponse.asMap();
+      settingsCubit.setSettings(settingsMap);
+      
       if (isFirstTime) {
         emit(SplashFirstTimeUser());
       } else if (token == null || token.isEmpty) {
         emit(SplashLoggedOut());
       } else {
-        emit(SplashLoggedIn());
+        emit(SplashLoggedIn(settingsMap));
       }
     } catch (e) {
       emit(SplashError("Something went wrong: ${e.toString()}"));

e9cc72fedaee2f67a4adb321345d5beb71de52d1
Master Daniel <engrdanywiss@gmail.com>
2025-10-04 11:20:22 +0100

    Removes redundant Key? parameter
    Removes the now redundant `Key? key` parameter from the `BottomSheetHandlebar` constructor.
    
    This simplifies the constructor and aligns with current best practices for Flutter widget keys.

--- lib/core/widgets/bottomsheet_handlebar.dart
+++ lib/core/widgets/bottomsheet_handlebar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BottomSheetHandlebar extends StatelessWidget {
-  const BottomSheetHandlebar({Key? key}) : super(key: key);
+  const BottomSheetHandlebar({super.key});
 
   @override
   Widget build(BuildContext context) {

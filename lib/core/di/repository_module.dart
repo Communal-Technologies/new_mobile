1c2db7c37eb45d957accf798291b694501f451c6
Master Daniel <engrdanywiss@gmail.com>
2025-10-04 11:18:46 +0100

    Adds repository module
    Sets up the repository module for dependency injection.
    
    This module provides the `AuthRepository` as a lazy singleton,
    making it readily available for use throughout the application.

---
+++ lib/core/di/repository_module.dart
@@ -0,0 +1,10 @@
+import 'package:communal_mobile/data/repositories/auth_repository.dart';
+import 'package:injectable/injectable.dart';
+import 'package:communal_mobile/data/datasources/remote/dio/dio_client.dart';
+
+@module
+abstract class RepositoryModule {
+  @lazySingleton
+  AuthRepository provideAuthRepository(DioClient dioClient) =>
+      AuthRepository(dioClient);
+}

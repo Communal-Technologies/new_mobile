56fc80ffba25d396919d6067933fd976a4ab72d2
Master Daniel <engrdanywiss@gmail.com>
2025-10-04 11:16:43 +0100

    Simplifies network module configuration
    Removes unnecessary dependencies and configuration from the network module.
    
    This streamlines the dependency injection setup and improves the overall modularity of the application.

--- lib/core/di/network_module.dart
+++ lib/core/di/network_module.dart
@@ -1,13 +1,10 @@
 import 'package:communal_mobile/core/constants/constants.dart';
-import 'package:communal_mobile/data/repositories/auth_repository.dart';
-import 'package:communal_mobile/cubits/splash/splash_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:injectable/injectable.dart';
 import 'package:communal_mobile/data/datasources/remote/dio/logging_interceptor.dart';
 import 'package:communal_mobile/data/datasources/remote/dio/dio_client.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 
 @module
 abstract class NetworkModule {
@@ -18,10 +15,7 @@ abstract class NetworkModule {
   LoggingInterceptor loggingInterceptor() => LoggingInterceptor();
 
   @lazySingleton
-  DioClient dioClient(
-    Dio dio,
-    LoggingInterceptor loggingInterceptor,
-  ) {
+  DioClient dioClient(Dio dio, LoggingInterceptor loggingInterceptor) {
     final baseUrl = dotenv.env['APP_ENV'] == 'development'
         ? dotenv.env['BASE_URL']!
         : AppConstants.baseUrl;
@@ -32,18 +26,4 @@ abstract class NetworkModule {
       customDio: dio,
     );
   }
-
-  @lazySingleton
-  AuthRepository provideAuthRepository(DioClient dioClient) =>
-      AuthRepository(dioClient);
-
-  @lazySingleton
-  SplashCubit provideSplashCubit(SharedPreferences prefs, DioClient dioClient) =>
-      SplashCubit(prefs, dioClient);
-
-  @lazySingleton
-  FlutterSecureStorage secureStorage() => const FlutterSecureStorage();
-
-  @preResolve
-  Future<SharedPreferences> prefs() => SharedPreferences.getInstance();
 }

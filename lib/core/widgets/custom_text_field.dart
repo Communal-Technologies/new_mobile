4fc336e327f3cfdc63aa13ea64a4648d44a52b4b
Master Daniel <engrdanywiss@gmail.com>
2025-10-27 06:37:51 +0100

    Adds custom text field widget
    Implements a reusable custom text field widget with various styling and validation options.
    
    This widget provides enhanced customization compared to the standard Flutter TextField, including:
    
    - Label and error text display
    - Prefix and suffix icons
    - Obscure text functionality
    - Keyboard type and text input action configuration
    - Validation support
    - Custom styling for different states (enabled, focused, error)

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    this.hintText,
+    this.labelText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.keyboardType,
+    this.textInputAction,
+    this.validator,
+    this.onChanged,
+    this.errorText,
+    this.enabled = true,
+    this.maxLength,
+    this.inputFormatters,
+  });
+
+  final TextEditingController controller;
+  final String? hintText;
+  final String? labelText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final String? errorText;
+  final bool enabled;
+  final int? maxLength;
+  final List<TextInputFormatter>? inputFormatters;
+
+  @override
+  Widget build(BuildContext context) {
+    final hasError = errorText != null && errorText!.isNotEmpty;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (labelText != null) ...[
+          Text(
+            labelText!,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w500,
+              color: Colors.black87,
+            ),
+          ),
+          SizedBox(height: 8.h),
+        ],
+        TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          keyboardType: keyboardType,
+          textInputAction: textInputAction,
+          enabled: enabled,
+          maxLength: maxLength,
+          inputFormatters: inputFormatters,
+          onChanged: onChanged,
+          style: TextStyle(
+            fontSize: 14.sp,
+            color: Colors.black,
+          ),
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: Colors.grey.shade400,
+              fontSize: 14.sp,
+            ),
+            prefixIcon: prefixIcon,
+            suffixIcon: suffixIcon,
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 12.w,
+              vertical: 12.h,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.r),
+              borderSide: BorderSide(
+                color: hasError ? Colors.red : Colors.grey.shade300,
+                width: 1.5,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.r),
+              borderSide: BorderSide(
+                color: hasError ? Colors.red : Colors.grey.shade300,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.r),
+              borderSide: BorderSide(
+                color: hasError
+                    ? Colors.red
+                    : Theme.of(context).primaryColor,
+                width: 1.5,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.r),
+              borderSide: const BorderSide(
+                color: Colors.red,
+                width: 1.5,
+              ),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.r),
+              borderSide: const BorderSide(
+                color: Colors.red,
+                width: 1.5,
+              ),
+            ),
+            counterText: '',
+          ),
+          validator: validator,
+        ),
+        if (hasError) ...[
+          SizedBox(height: 4.h),
+          Text(
+            errorText!,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 12.sp,
+            ),
+          ),
+        ],
+      ],
+    );
+  }
+}
+

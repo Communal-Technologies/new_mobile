b0eada632173184ea9e207a7e4d5123b37c23904
Master Daniel <engrdanywiss@gmail.com>
2025-11-15 20:08:35 +0100

    Adds transaction tile widget
    Creates a reusable transaction tile widget for displaying transaction details.
    
    The widget includes an icon, title, subtitle, and amount, and is designed to be tappable.

---
+++ lib/screens/transactions/widgets/transaction_tile.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:communal_mobile/core/widgets/space.dart';
+import 'package:communal_mobile/screens/transactions/models/sample_transactions.dart';
+
+class TransactionTile extends StatelessWidget {
+  const TransactionTile({super.key, required this.item, this.onTap});
+
+  final TransactionListItem item;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12.r),
+      child: Container(
+        padding: EdgeInsets.all(14.w),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 48.w,
+              height: 48.w,
+              decoration: BoxDecoration(
+                color: item.iconBgColor,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(item.icon, color: item.iconColor, size: 24.sp),
+            ),
+            hSpace(12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.title,
+                    style: TextStyle(
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  vSpace(4),
+                  Text(
+                    item.subtitle,
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              item.signedAmountLabel,
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w700,
+                color: item.amountColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

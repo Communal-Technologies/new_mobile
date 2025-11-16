3850f4556ddb68469e49f65bed75eff1d8e8c8d8
Master Daniel <engrdanywiss@gmail.com>
2025-11-16 21:47:16 +0100

    Adds setting item widget
    Implements a reusable setting item widget for the account screen.
    
    This widget provides a consistent UI for displaying settings with an icon, title, description, and an optional trailing widget. It also includes tap functionality for user interaction.

---
+++ lib/screens/account/widgets/setting_item.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:communal_mobile/core/widgets/space.dart';
+
+class SettingItem extends StatelessWidget {
+  const SettingItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+    required this.onTap,
+    this.trailing,
+  });
+
+  final IconData icon;
+  final String title;
+  final String description;
+  final VoidCallback onTap;
+  final Widget? trailing;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.only(bottom: 1.h),
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+        color: Colors.white,
+        child: Row(
+          children: [
+            Container(
+              width: 40.w,
+              height: 40.w,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              child: Icon(
+                icon,
+                color: const Color(0xFF7434FF),
+                size: 20.sp,
+              ),
+            ),
+            hSpace(16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF0F1D40),
+                    ),
+                  ),
+                  vSpace(4),
+                  Text(
+                    description,
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (trailing != null)
+              trailing!
+            else
+              Icon(
+                Icons.chevron_right,
+                color: Colors.grey.shade400,
+                size: 20.sp,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+

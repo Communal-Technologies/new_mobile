d0bdb0ea17fab0b0beda719df36b95d920c232c7
Master Daniel <engrdanywiss@gmail.com>
2025-11-16 21:49:04 +0100

    Adds account settings navigation
    Adds navigation to the account settings screen from the community map screen.
    This allows users to easily access their account settings.

--- lib/screens/community/community_map_screen.dart
+++ lib/screens/community/community_map_screen.dart
@@ -150,6 +150,9 @@ class _CommunityMapScreenState extends State<CommunityMapScreen> {
             case 3:
               context.goNamed('loans');
               break;
+            case 4:
+              context.goNamed('account-settings');
+              break;
             default:
               setState(() => _currentIndex = index);
           }

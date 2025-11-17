71c74dfb8cbfbb5b93c112df3b6592d6d890e50e
Master Daniel <engrdanywiss@gmail.com>
2025-11-17 03:42:32 +0100

    Adds community action item widget
    Adds a reusable widget for displaying community action items.
    
    This widget includes an icon, title, description, and tap action,
    and is styled for use in account or settings screens.

---
+++ lib/screens/account/widgets/community_action_item.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:communal_mobile/core/widgets/space.dart';
+
+class CommunityActionItem extends StatelessWidget {
+  const CommunityActionItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+    required this.iconColor,
+    required this.textColor,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String title;
+  final String description;
+  final Color iconColor;
+  final Color textColor;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.only(bottom: 1.h),
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+        color: Colors.white,
+        child: Row(
+          children: [
+            Container(
+              width: 40.w,
+              height: 40.w,
+              decoration: BoxDecoration(
+                color: iconColor.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              child: Icon(
+                icon,
+                color: iconColor,
+                size: 20.sp,
+              ),
+            ),
+            hSpace(16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w600,
+                      color: textColor,
+                    ),
+                  ),
+                  vSpace(4),
+                  Text(
+                    description,
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.chevron_right,
+              color: Colors.grey.shade400,
+              size: 20.sp,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+

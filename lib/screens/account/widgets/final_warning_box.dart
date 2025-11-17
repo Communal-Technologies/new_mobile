14ee5d11d90854e39f4acf94b4adee64e7578b75
Master Daniel <engrdanywiss@gmail.com>
2025-11-17 04:07:06 +0100

    Adds final warning box widget
    Adds a widget to display a final warning message before account deletion.
    
    This widget informs the user about the irreversible nature of account deletion, ensuring they understand the consequences before proceeding.

---
+++ lib/screens/account/widgets/final_warning_box.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:communal_mobile/core/widgets/space.dart';
+
+class FinalWarningBox extends StatelessWidget {
+  const FinalWarningBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.w),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFEBEE), // Light red
+        borderRadius: BorderRadius.circular(12.r),
+        border: Border.all(
+          color: const Color(0xFFD32F2F), // Red
+          width: 1,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Are You Absolutely Sure?',
+            style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w700,
+              color: const Color(0xFFD32F2F),
+            ),
+          ),
+          vSpace(8),
+          Text(
+            'Once you click "Delete My Account permanently", your account and all data will be permanently deleted. This action cannot be reversed.',
+            style: TextStyle(
+              fontSize: 14.sp,
+              color: const Color(0xFF0F1D40),
+              height: 1.5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+

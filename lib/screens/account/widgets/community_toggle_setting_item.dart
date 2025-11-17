be95cb77981870278d0c6335c0ba34c5723a15cf
Master Daniel <engrdanywiss@gmail.com>
2025-11-17 03:53:37 +0100

    Adds community toggle setting item widget
    Adds a new widget for displaying community-related toggle settings.
    
    This widget provides a consistent UI element for managing community settings, including an icon, title, description, and toggle switch. It also supports disabling the toggle based on the `enabled` parameter.

---
+++ lib/screens/account/widgets/community_toggle_setting_item.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:communal_mobile/core/widgets/space.dart';
+
+class CommunityToggleSettingItem extends StatelessWidget {
+  const CommunityToggleSettingItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+    required this.value,
+    required this.onChanged,
+    this.enabled = true,
+  });
+
+  final IconData icon;
+  final String title;
+  final String description;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 1.h),
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+      color: Colors.white,
+      child: Row(
+        children: [
+          Container(
+            width: 40.w,
+            height: 40.w,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.circular(10.r),
+            ),
+            child: Icon(
+              icon,
+              color: enabled
+                  ? (value ? const Color(0xFF7434FF) : Colors.grey.shade600)
+                  : Colors.grey.shade400,
+              size: 20.sp,
+            ),
+          ),
+          hSpace(16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 15.sp,
+                    fontWeight: FontWeight.w600,
+                    color: enabled
+                        ? const Color(0xFF0F1D40)
+                        : Colors.grey.shade400,
+                  ),
+                ),
+                vSpace(4),
+                Text(
+                  description,
+                  style: TextStyle(
+                    fontSize: 13.sp,
+                    color: enabled
+                        ? Colors.grey.shade600
+                        : Colors.grey.shade400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Switch(
+            value: value,
+            onChanged: enabled ? onChanged : null,
+            activeColor: const Color(0xFF7434FF),
+          ),
+        ],
+      ),
+    );
+  }
+}
+

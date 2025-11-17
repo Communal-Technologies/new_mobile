2d1c9422e404118757be85ad28c6b2461ffdf47f
Master Daniel <engrdanywiss@gmail.com>
2025-11-17 03:55:35 +0100

    Adds data loss item widget
    Adds a reusable widget to display potential data loss scenarios with an icon, title, and description, enhancing the user interface for account security information.

---
+++ lib/screens/account/widgets/data_loss_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:communal_mobile/core/widgets/space.dart';
+
+class DataLossItem extends StatelessWidget {
+  const DataLossItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+    required this.iconColor,
+  });
+
+  final IconData icon;
+  final String title;
+  final String description;
+  final Color iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 40.w,
+          height: 40.w,
+          decoration: BoxDecoration(
+            color: iconColor.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(10.r),
+          ),
+          child: Icon(
+            icon,
+            color: iconColor,
+            size: 20.sp,
+          ),
+        ),
+        hSpace(12),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w600,
+                  color: const Color(0xFF0F1D40),
+                ),
+              ),
+              vSpace(4),
+              Text(
+                description,
+                style: TextStyle(
+                  fontSize: 13.sp,
+                  color: Colors.grey.shade600,
+                  height: 1.4,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+

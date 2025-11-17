b0edbdcd33d368e9231530eb4e7eed8768598938
Master Daniel <engrdanywiss@gmail.com>
2025-11-17 04:08:52 +0100

    Adds FreezeAccountHeader widget
    Adds a reusable widget for displaying a header with an icon, title, and description, specifically designed for freeze account screens.

---
+++ lib/screens/account/widgets/freeze_account_header.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:communal_mobile/core/widgets/space.dart';
+
+class FreezeAccountHeader extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String description;
+
+  const FreezeAccountHeader({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          width: 80.w,
+          height: 80.w,
+          decoration: BoxDecoration(
+            color: const Color(0xFF7434FF).withOpacity(0.1),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(
+            icon,
+            color: const Color(0xFF7434FF),
+            size: 50.sp,
+          ),
+        ),
+        vSpace(24),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w700,
+            color: const Color(0xFF0F1D40),
+          ),
+        ),
+        vSpace(12),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 14.sp,
+            color: Colors.grey.shade600,
+            height: 1.5,
+          ),
+        ),
+      ],
+    );
+  }
+}
+

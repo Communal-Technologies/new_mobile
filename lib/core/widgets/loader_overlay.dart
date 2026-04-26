4b8eca575f1c9c6e3cdece50d78f5640c8acdbaf
Master Daniel <engrdanywiss@gmail.com>
2026-04-26 03:50:30 +0100

    feat(loader_overlay): replace static loader image with animated logo and add customizable loader size
    - Updated LoaderOverlay to use AnimatedLogoLoader instead of a static image, enhancing visual feedback.
    - Introduced loaderSize parameter to allow customization of the loader's size, improving flexibility in UI design.

--- lib/core/widgets/loader_overlay.dart
+++ lib/core/widgets/loader_overlay.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:communal_mobile/core/constants/images.dart';
+import 'package:communal_mobile/core/widgets/animated_logo_loader.dart';
 
 /// Full-screen dimming layer + loader. Blocks taps to widgets below (use inside [Stack] with [Positioned.fill]).
 class LoaderOverlay extends StatelessWidget {
-  const LoaderOverlay({super.key});
+  const LoaderOverlay({super.key, this.loaderSize = 52});
+
+  final double loaderSize;
 
   @override
   Widget build(BuildContext context) {
     return AbsorbPointer(
       child: Material(
-        color: Colors.black.withValues(alpha: 0.35),
+        color: Colors.black,
         child: SizedBox.expand(
           child: Center(
-            child: Image.asset(
-              Images.loader,
-              width: 80.w,
-              height: 80.w,
-            ),
+            child: AnimatedLogoLoader(size: loaderSize),
           ),
         ),
       ),

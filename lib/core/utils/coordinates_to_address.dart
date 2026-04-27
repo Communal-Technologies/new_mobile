00dfd191ea8d16f6c42a5fd900bf86292614d40e
Master-Daniel <engrdanywiss@gmail.com>
2026-04-27 15:16:05 +0000

    refactor(coordinates_to_address.dart): clean up imports and streamline geocoding functions
    - Removed unnecessary dotenv import and streamlined the geocoding functions for better readability.
    - Ensured consistent usage of the LocatitonGeocoder initialization across both address and city retrieval methods.
    - Maintained the existing functionality while improving code clarity and organization.

--- lib/core/utils/coordinates_to_address.dart
+++ lib/core/utils/coordinates_to_address.dart
@@ -1,11 +1,8 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:location_geocoder/location_geocoder.dart';
 
 import '../constants/constants.dart';
 
 Future<String> getAddressFromCoordinates({required double long, required double lat}) async {
-  var apiKey = dotenv.env['GOOGLE_MAPS_API_KEY'];
-
   final LocatitonGeocoder geocoder = LocatitonGeocoder(AppConstants.googleMapsApiKey);
   final address = await geocoder.findAddressesFromCoordinates(Coordinates(lat, long));
 
@@ -13,8 +10,6 @@ Future<String> getAddressFromCoordinates({required double long, required double 
 }
 //TODO - MAKE ONE CLASS
 Future<String> getCityFromCoordinates(double latitude, double longitude) async {
-  var apiKey = dotenv.env['GOOGLE_MAPS_API_KEY'];
-
   final LocatitonGeocoder geocoder =
       LocatitonGeocoder(AppConstants.googleMapsApiKey);
   final address = await geocoder

9fc07a6e4f6391f10bdcab0733351263cd75ba7e
Master Daniel <engrdanywiss@gmail.com>
2026-06-18 09:36:24 +0100

    refactor(set-pin-screen): change app bar background to transparent for improved UI consistency

--- lib/screens/auth/set_pin_screen.dart
+++ lib/screens/auth/set_pin_screen.dart
@@ -113,7 +113,7 @@ class _SetPinScreenState extends State<SetPinScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).cardColor,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).cardColor,
+        backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),

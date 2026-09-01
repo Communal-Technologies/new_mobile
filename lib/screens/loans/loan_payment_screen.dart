fdeb4d1d734f3d3eee3c8f949c717eb16fa6a98d
Master Daniel <engrdanywiss@gmail.com>
2026-09-01 08:03:07 +0100

    Respect the symbol position on the loan payment input

--- lib/screens/loans/loan_payment_screen.dart
+++ lib/screens/loans/loan_payment_screen.dart
@@ -418,6 +418,8 @@ class _LoanPaymentScreenState extends State<LoanPaymentScreen> {
   Widget _buildAmountInput() {
     final currency = widget.loan.currency;
     final decimals = decimalsFor(currency);
+    final display = activeCurrency.display.forCurrency(currency);
+    final onLeft = display.position == CurrencySymbolPosition.left;
     final allowDecimal = decimals > 0;
     final decimalSeparators = allowDecimal ? r'\.,' : '';
     return Column(
@@ -441,7 +443,8 @@ class _LoanPaymentScreenState extends State<LoanPaymentScreen> {
             ),
           ],
           decoration: InputDecoration(
-            prefixText: '${currencySymbolForCode(currency)} ',
+            prefixText: onLeft ? '${display.symbol} ' : null,
+            suffixText: onLeft ? null : ' ${display.symbol}',
             hintText: Money(
               widget.loan.monthlyRepaymentMinor,
               currency,

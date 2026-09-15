f461d6556a5e15567bdb7145440f95a77636c388
Master Daniel <engrdanywiss@gmail.com>
2026-09-15 07:23:50 +0100

    Pass the takeover countdowns from login to the pending state

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -221,6 +221,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
             otpChannel: loginResponse.otpChannel ?? 'phone',
             login: event.login.trim(),
             message: loginResponse.message,
+            otpExpiresIn: loginResponse.otpExpiresIn,
+            resendAvailableIn: loginResponse.resendAvailableIn,
           ));
           return;
         }
